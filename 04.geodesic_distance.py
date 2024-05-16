d32e288b20f38b1d2001e529308a4e3372e67772
alberti-f <fnc.alberti@gmail.com>
2024-05-16 15:54:35 +0200

    compute group dists separately

--- 04.geodesic_distance.py
+++ 04.geodesic_distance.py
@@ -1,4 +1,4 @@
-# 04 Compute geodesic dist mats
+# 04 Compute geodesic distance mats
 
 import nibabel as nib
 from subprocess import run
@@ -13,22 +13,6 @@ data = dataset()
 id = data.subj_list[index]
 
 command = "wb_command -surface-geodesic-distance-all-to-all {0} {1}"
-
-
-# Compute geodesic distance on group surface
-for h in ["L", "R"]:
-
-    filename = data.outpath(f"{data.id}.{h}.gdist_triu.10k_fs_LR.npy")
-    if exists(filename):
-        continue
-
-    surface = getattr(data, f"{h}_cortex_midthickness_10k")
-    dconn = data.outpath(f"{data.id}.{h}.geodesic_distance.dconn.nii")
-    run(command.format(surface, dconn), shell=True)
-    gdist_matrix = nib.load(dconn)
-
-    np.save(filename, gdist_matrix.get_fdata(caching="unchanged")[np.triu_indices_from(gdist_matrix, k=1)].astype("float32"))
-
 
 subj = subject(id)
 
