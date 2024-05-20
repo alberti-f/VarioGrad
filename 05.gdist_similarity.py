c8dc03ee503356cc9deb8de47955b85e854a9001
alberti-f <fnc.alberti@gmail.com>
2024-05-20 17:25:59 +0200

    fixed parallelization error

--- 05.gdist_similarity.py
+++ 05.gdist_similarity.py
@@ -1,4 +1,4 @@
-# inter-subject similarity of vertex geodesic distance profile
+# inter-subject similarity of vertex geodesic distance profile in parallel
 
 import numpy as np
 from variograd_utils import *
@@ -7,28 +7,24 @@ from sklearn.metrics.pairwise import euclidean_distances
 from joblib import Parallel, delayed
 
 data =  dataset()
-data.subj_list = data.subj_list
-n_subj = data.N
 
-
-subj_pairs = list(combinations(range(n_subj), 2))
-subj_pairs = data.subj_list[subj_pairs]
+subj_pairs = list(combinations(data.subj_list, 2))
 
 print(f"\n\nComputing the inter-subject similarity of vertex geodesic distance profile for {len(subj_pairs)} subject pairs \n")
 
 
-gdist_L2_l = Parallel(n_jobs=-1)(delayed(np.diag)(euclidean_distances(subject(i).load_gdist_matrix("L")),
-                                                  euclidean_distances(subject(j).load_gdist_matrix("L"))) 
-                                                  for i, j in subj_pairs)
+gdist_L2_l = Parallel(n_jobs=-1)(delayed(np.diag)(euclidean_distances(subject(i).load_gdist_matrix("L"), 
+                                                                      subject(j).load_gdist_matrix("L"))) 
+                                                                      for i, j in subj_pairs)
 filename = data.outpath("AllToAll.L.gdist_L2.npy")
 np.save(filename, np.asarray(gdist_L2_l).T)
 print(f"Left hemisphere output saved at {filename} \n", 
       f"Matrix size: {np.asarray(gdist_L2_l).T.shape} \n")
 
 
-gdist_L2_r = Parallel(n_jobs=-1)(delayed(np.diag)(euclidean_distances(subject(i).load_gdist_matrix("R")),
-                                                  euclidean_distances(subject(j).load_gdist_matrix("R")))
-                                                  for i, j in subj_pairs)
+gdist_L2_r = Parallel(n_jobs=-1)(delayed(np.diag)(euclidean_distances(subject(i).load_gdist_matrix("R"),
+                                                                      subject(j).load_gdist_matrix("R"))) 
+                                                                      for i, j in subj_pairs)
 filename = data.outpath("AllToAll.R.gdist_L2.npy")
 np.save(filename, np.asarray(gdist_L2_r).T)
 print(f"Right hemisphere output saved at {filename} \n", 
