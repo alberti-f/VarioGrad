aa9c1d84b9f0e0c442d23f875d762e918e8032a7
alberti-f <fnc.alberti@gmail.com>
2024-05-24 12:11:42 +0200

    add garbage collecion

--- 06.joint_embedding.py
+++ 06.joint_embedding.py
@@ -1,9 +1,10 @@
 # Joint Embedding in parallel
-
+import psutil
 from variograd_utils import *
 from nighres.shape import spectral_matrix_embedding
 import numpy as np
 from joblib import Parallel, delayed
+import gc
 
 n_components = 10
 data = dataset()
@@ -16,6 +17,7 @@ params = np.array(np.meshgrid(hemi, msize, scale, affinity), dtype="object").T.r
 
 def joint_embedding(id, h, m, s, a):
 
+    print("\n\n\t\t\tSubject ID: ", id, "\n\n")    
     subj = subject(id)
     subj_points = subj.load_surf(h, 10, type="cortex_midthickness").darrays[0].data
     avg_points = data.load_surf(h, 10, type="cortex_midthickness").darrays[0].data
@@ -33,19 +35,32 @@ def joint_embedding(id, h, m, s, a):
                                             reference_mesh = getattr(data, f"{h}_cortex_midthickness_10k"), 
                                             dims=n_components, msize=m, scale=s, space=s, affinity=a, 
                                             rotate=True, save_data=False, overwrite=True)
+    
+    del subj_points, avg_points, C, R, M
+    gc.collect()
+
     return embedding["result"]
 
 
 for n, args in enumerate(params):
+    process = psutil.Process()
+
     h, m, s, a  = args
 
     print(f"\n\n\t\t\tParameter combinaiton {n+1}/{len(params)}\n", h, m, s, a)
 
     n_vertices = vertex_info_10k[f"gray{h.lower()}"].size
 
-    all_embeddings = Parallel(n_jobs=-1)(delayed(joint_embedding)(id, h, m, s, a) for id in data.subj_list)
+    all_embeddings = Parallel(n_jobs=3)(delayed(joint_embedding)(id, h, m, s, a) for id in data.subj_list)
 
     filename = data.outpath(f'All.{h}.embeddings.npz')
     npz_update(filename, {f"JE_m{int(m)}_{a}_s{int(s)}": all_embeddings})
 
     print(f"Output saved in archive {filename} \nFile name: JE_m{int(m)}_{a}_s{int(s)} \n")
+
+    print("Peak memory usage: ", process.memory_info().rss / 1e+9, "GB")
+    gc.collect()
+    print("Memory usage after garbage collection: ", process.memory_info().rss / 1e+9, "GB")
+    print("\n\n")
+    
+    del all_embeddings
