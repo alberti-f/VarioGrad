b407a24089a6e4f21a1248a1915e38c3b2d4187f
alberti-f <fnc.alberti@gmail.com>
2024-05-30 11:24:57 +0200

    concatenate individual embeddings

---
+++ concatenate_embeddings.py
@@ -0,0 +1,46 @@
+from surfplot import Plot
+from variograd_utils import *
+import numpy as np
+import os
+
+
+n_components = 10
+data = dataset()
+embeddings = data.load_embeddings("L", "JDE")["JDE_cauchy50_a0.5_t1"]
+embeddings.shape
+
+
+# preassign empty arrays
+zeroarrayl = np.zeros([data.N, vertex_info_10k.grayl.size, n_components])
+zeroarrayr = np.zeros([data.N, vertex_info_10k.grayr.size, n_components])
+
+
+# add subject embeddings to the group dictionary
+embeddings_l = {}
+embeddings_r = {}
+for i, id in enumerate(data.subj_list):
+
+    # embeddings_subj = np.load(data.outpath(f"{id}.L.embeddings.npz"))
+
+    # for key, value in embeddings_subj.items():
+    #     if key not in embeddings_l:
+    #         embeddings_l[key] = zeroarrayl.copy()
+    #     embeddings_l[key][i] = value
+
+
+    embeddings_subj = np.load(data.outpath(f"{id}.R.embeddings.npz"))
+
+    for key, value in embeddings_subj.items():
+        if key not in embeddings_r:
+            embeddings_r[key] = zeroarrayr.copy()
+        embeddings_r[key][i] = value
+    
+
+    # remove subject embeddings
+    # os.remove(subj.outpath(f"{id}.L.embeddings.npz"))
+    # os.remove(subj.outpath(f"{id}.R.embeddings.npz"))
+
+
+# save group embeddings
+# npz_update(data.outpath("All.L.embeddings.npz"), embeddings_l)
+npz_update(data.outpath("All.R.embeddings.npz"), embeddings_r)
