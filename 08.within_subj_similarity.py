d0e9958db6a871558f5294ecd08f307ee103f08e
alberti-f <fnc.alberti@gmail.com>
2024-06-06 12:14:53 +0200

    compare w/in subject similarity

--- 08.within_subj_similarity.py
+++ 08.within_subj_similarity.py
@@ -18,6 +18,7 @@ print("\tradius of the comparisons:", radius)
 print("\tN vertices sampled per radius:", size)
 
 data =  dataset()
+
 embed_l, embed_r = (data.load_embeddings("L", algorithm), data.load_embeddings("R", algorithm))
 
 n_comps = embed_l[list(embed_l.keys())[0]].shape[2]
@@ -26,7 +27,7 @@ vinfo = vertex_info_10k
 
 
 # Check for the existence of previous computations and avoit overwriting if overwrite is False
-skip_algs_l = skip_algs_r =[]
+skip_algs_l = skip_algs_r = []
 if exists(data.outpath("All.L.within_subj_similarity.npz")) and not overwrite:
     skip_algs_l = np.load(data.outpath(f"All.L.within_subj_similarity.npz")).keys()
 if exists(data.outpath("All.R.within_subj_similarity.npz")) and not overwrite:
@@ -67,7 +68,7 @@ for id in data.subj_list:
         if k in skip_algs_l and not overwrite:
             continue
 
-        edist_matrix = euclidean_distances(subj.load_embeddings("L", k)[k]).astype("float32")
+        edist_matrix = euclidean_distances(embed_l[k][subj.idx]).astype("float32")
 
         for i, r in enumerate(radius):
 
@@ -83,7 +84,7 @@ for id in data.subj_list:
         if k in skip_algs_r and not overwrite:
             continue
 
-        edist_matrix = euclidean_distances(subj.load_embeddings("R", k)[k]).astype("float32")
+        edist_matrix = euclidean_distances(embed_r[k][subj.idx]).astype("float32")
 
         for i, r in enumerate(radius):
 
@@ -118,26 +119,26 @@ from statsmodels.stats.multitest import fdrcorrection
 alpha = 0.05
 
 data = dataset()
-alg_pairs = combinations(algorithm, 2)
+alg_pairs = list(combinations(algorithm, 2))
 
 print("\n\nComparing the within-subject similarity of vertex distances in physical \nand latent space between algorithms.", 
-      f"\n Compared algorighms: ", "\n\t".join(algorithm))
+      f"\n Compared algorighms:\n\t", "\n\t".join(algorithm))
 
 for h in ["L", "R"]:
     t_maps = {}
     p_maps = {}
 
-        
+    # find pairs of parameters to compare between algorithms
     correlations = np.load(data.outpath(f"All.{h}.within_subj_similarity.npz"))
-
     param_pairs = []
     for alg_i, alg_j in alg_pairs:
         param_pairs.extend([ (i, j) for i in correlations.keys() for j in correlations.keys()
-                            if i.startswith(alg_i) and j.startswith(alg_j)])
+                            if i.startswith(alg_i) and j.startswith(alg_j) ])
 
 
     print(f"\nHemisphere:", h, "\nN comparisons:", len(param_pairs))
 
+    # t-test for each pair of parameters
     for param_i, param_j in param_pairs: 
         X = correlations[param_i]
         Y = correlations[param_j]
@@ -146,6 +147,10 @@ for h in ["L", "R"]:
 
         t_maps[f"{param_i}-vs-{param_j}"] = results.statistic
         p_maps[f"{param_i}-vs-{param_j}"] = pvalues
+
+        print(f"\t{param_i} vs {param_j}:",
+              f"\t\t Percent significant: \t{np.sum(pvalues<alpha) / pvalues.size}%",
+              f"\t\t Average t-value: \t{np.nanmean(results.statistic[pvalues<alpha])}")
 
     npz_update(data.outpath(f"{data.id}.{h}.wss_t_maps.npz"), t_maps)
     npz_update(data.outpath(f"{data.id}.{h}.wss_p_maps.npz"), p_maps)
