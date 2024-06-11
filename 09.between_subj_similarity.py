f807f8c1e67390740ccd6a82f616f454a3f502f7
alberti-f <fnc.alberti@gmail.com>
2024-06-11 14:41:51 +0200

    do L and R separately

--- 09.between_subj_similarity.py
+++ 09.between_subj_similarity.py
@@ -31,30 +31,38 @@ subj_pair = data.pairs[pair_idx]
 subj_i = subject(subj_pair[0])
 subj_j = subject(subj_pair[1])
 
-# # Wait until all subject pairs are done
-# if pair_idx == len(data.pairs)-1:
-#     run_last = False
-#     while not run_last:
-#         sleep(60)
-#         run_last = all([os.path.exists(data.outpath(f"{i}-{j}.R.embed_similarity.npz")) 
-#                         for i, j in data.pairs])
 
-# correlations_l = {}
-# correlations_r = {}
-# for k in embed_l.keys():
+print("\n\nSubject pair:", subj_i.id, subj_j.id)
 
-#     if k in skip_algs_l and not overwrite:
-#         print(f"{k} skipped")
-#         continue
+# Wait until all subject pairs are done
+if pair_idx == len(data.pairs)-1:
+    print("\nWaiting for all pairs to be done before running the last one")
+    run_last = False
+    while not run_last:
+        print("\t...")
+        sleep(30)
+        run_last = all([os.path.exists(data.outpath(f"{i}-{j}.R.embed_similarity.npz")) 
+                        for i, j in data.pairs])
+    
+sleep(30)
 
-#     correlations_l[k] = vector_wise_corr(embed_l[k][subj_i.idx].T, embed_l[k][subj_j.idx].T).astype("float32")
-#     correlations_r[k] = vector_wise_corr(embed_r[k][subj_i.idx].T, embed_r[k][subj_j.idx].T).astype("float32")
+print("\n\nComputing correlation of embeddings\n")
+correlations_l = {}
+correlations_r = {}
+for k in embed_l.keys():
 
-#     print(f"{k} completed")
+    if k in skip_algs_l and not overwrite:
+        print(f"\t{k} skipped")
+        continue
+
+    correlations_l[k] = vector_wise_corr(embed_l[k][subj_i.idx].T, embed_l[k][subj_j.idx].T).astype("float32")
+    correlations_r[k] = vector_wise_corr(embed_r[k][subj_i.idx].T, embed_r[k][subj_j.idx].T).astype("float32")
+
+    print(f"\t{k} completed")
 
 
-# npz_update(data.outpath(f"{subj_i.id}-{subj_j.id}.L.embed_similarity.npz"), correlations_l)
-# npz_update(data.outpath(f"{subj_i.id}-{subj_j.id}.R.embed_similarity.npz"), correlations_r)
+npz_update(data.outpath(f"{subj_i.id}-{subj_j.id}.L.embed_similarity.npz"), correlations_l)
+npz_update(data.outpath(f"{subj_i.id}-{subj_j.id}.R.embed_similarity.npz"), correlations_r)
 
 
 ###################################################################################
@@ -67,6 +75,7 @@ algorithms = list(embed_l.keys())
 filenames_l = [data.outpath(f"All.L.bwsim.{k}.npy") for k in algorithms]
 filenames_r = [data.outpath(f"All.R.bwsim.{k}.npy") for k in algorithms]
 
+# Preallocating files
 print("Preallocating empty files:\n")
 for f in filenames_l:
     if exists(f) and not overwrite:
@@ -86,33 +95,45 @@ for f in filenames_r:
 
 
 
-print("Updating files:\n")
-for file in filenames_l[:1]:
+
+print("\n\nUpdating group files:\n")
+
+from datetime import datetime
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print(f"\n\tStart = {current_time}")
+
+
+print("\n\tLeft hemisphere")
+for file in filenames_l:
     bwsim_l = np.load(file, mmap_mode="r+")
 
-    for i, j in data.pairs[:3]:
+    for idx, (i, j) in enumerate(data.pairs):
         if not (exists(data.outpath(f"{i}-{j}.L.embed_similarity.npz")) and exists(data.outpath(f"{i}-{j}.R.embed_similarity.npz"))):
                 print(f"Missing pair {i}-{j}")
                 continue        
         
-        bwsim_l[pair_idx] = np.load(data.outpath(f"{i}-{j}.L.embed_similarity.npz"))[file.split(".")[-2]]
+        bwsim_l[idx] = np.load(data.outpath(f"{i}-{j}.L.embed_similarity.npz"))[file.split(".")[-2]]
 
     bwsim_l.flush()    
-    print(f"File {file} completed for L")
+    print(f"\t\t{file} done")
 
 
-
-for file in filenames_r[:1]:
+print("\n\tRight hemisphere")
+for file in filenames_r:
     bwsim_r = np.load(file, mmap_mode="r+")
 
-    for i, j in data.pairs[:3]:
+    for idx, (i, j) in enumerate(data.pairs):
         if not (exists(data.outpath(f"{i}-{j}.L.embed_similarity.npz")) and exists(data.outpath(f"{i}-{j}.R.embed_similarity.npz"))):
                 print(f"Missing pair {i}-{j}")
                 continue
         
-        bwsim_r[pair_idx] = np.load(data.outpath(f"{i}-{j}.R.embed_similarity.npz"))[file.split(".")[-2]]
+        bwsim_r[idx] = np.load(data.outpath(f"{i}-{j}.R.embed_similarity.npz"))[file.split(".")[-2]]
 
     bwsim_r.flush()    
-    print(f"File {file} completed for R")
+    print(f"\t\t{file} done")
 
 
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print(f"\n\tEnd = {current_time}\n")
