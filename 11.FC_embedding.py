7634a2b6912cae28e8368e7fe30485b30eb7f687
alberti-f <fnc.alberti@gmail.com>
2024-06-19 00:02:21 +0200

    comment out zscore info

--- 11.FC_embedding.py
+++ 11.FC_embedding.py
@@ -16,7 +16,7 @@ subj = subject(id)
 runs = ["REST1_LR", "REST2_RL"]
 tseries10k = subj.outpath(f"{id}.rfMRI_REST_Atlas_MSMAll.10k_fs_LR.dtseries.nii")
 tseries10k_gii = subj.outpath(f"{id}." + "{0}.rfMRI_{1}_Atlas_MSMAll.10k_fs_LR.func.gii")
-tseries32k = subj.dir + "/MNINonLinear/Results/rfMRI_{0}/rfMRI_{0}_Atlas_MSMAll_hp2000_clean.dtseries.nii" #rfMRI_{0}_Atlas_MSMAll
+tseries32k = subj.dir + "/MNINonLinear/Results/rfMRI_{0}/rfMRI_{0}_Atlas_MSMAll.nii" # rfMRI_{0}_Atlas_MSMAll_hp2000_clean.dtseries
 tseries32k_gii = subj.outpath(f"{id}." + "{0}.rfMRI_{1}_Atlas_MSMAll.32k_fs_LR.func.gii")
 sphere32k = data.group_dir + "/S1200.{0}.sphere.32k_fs_LR.surf.gii"
 sphere10k = data.mesh10k_dir + "/S1200.{0}.sphere.10k_fs_LR.surf.gii"
@@ -37,7 +37,7 @@ concat_R = f"wb_command -metric-merge {tseries10k_gii.format('R', 'REST')}"
 
 print("\n\nSplitting 32k timeseries into hemispheres:")
 for r in runs:
-    print(f"\tProcessing {r}")
+    print(f"\n\nProcessing {r}")
 
     separate_hemispheres = f"wb_command -cifti-separate {tseries32k.format(r)} COLUMN \
         -metric CORTEX_LEFT {tseries32k_gii.format('L', r)} \
@@ -62,18 +62,18 @@ for r in runs:
 
 
     # Normalize timeseries
-    print("\n\nNormalizing timeseries:")
+    print("Normalizing timeseries:")
     for h in ["L", "R"]:
 
         tseries = [t.data for t in nib.load(tseries10k_gii.format(h, r)).darrays]
         tseries_z = [z for z in zscore(np.array(tseries), axis=0, nan_policy='omit')]
         structure = "CORTEX_LEFT" if h == "L" else "CORTEX_RIGHT"
 
-        print(f"\n\tHemisphere:", h, "\tRun:", r)
-        print("\tt points:", len(tseries_z), 
-              "\tVertices:", len(tseries_z[0]),
-              "\tMean", np.nanmean(np.nanmean(tseries_z, axis=0)), 
-              "\tSD", np.nanmean(np.nanstd(tseries_z, axis=0))
+        print(f"Hemisphere:", h, "\tRun:", r)
+        # print("\tt points:", len(tseries_z), 
+        #       "\tVertices:", len(tseries_z[0]),
+        #       "\tMean", np.nanmean(np.nanmean(tseries_z, axis=0)), 
+        #       "\tSD", np.nanmean(np.nanstd(tseries_z, axis=0)))
    
         save_gifti(darrays=tseries_z, intents=11, dtypes=16, structure=structure, 
                    filename=tseries10k_gii.format(h, r), encoding=3, endian=2)
