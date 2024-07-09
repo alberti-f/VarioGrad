08b120e351088affbfee2cbd57c232a30f814724
alberti-f <fnc.alberti@gmail.com>
2024-07-10 01:32:39 +0200

    find nans in tseries

--- 12.joint_embedding_fc.py
+++ 12.joint_embedding_fc.py
@@ -19,6 +19,7 @@ W[W < np.percentile(W, threshold, axis=0)] = 0
 
 # Load individual timeseries and threshold
 M = nib.load(subject(subj.id).outpath(f"{subj.id}.rfMRI_REST_Atlas_MSMAll.10k_fs_LR.dtseries.nii")).get_fdata().astype("float32")[:, cortex]
+print(np.any(np.isnan(M)))
 M = np.corrcoef(M.T)
 M[M < 0] = 0
 M[M < np.percentile(M, threshold, axis=0)] = 0
