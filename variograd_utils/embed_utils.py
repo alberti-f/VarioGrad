728d3e07d942624d103f8c5b68c6526bd0a12ab7
alberti-f <fnc.alberti@gmail.com>
2024-07-09 23:33:30 +0200

    add diffusion map embedding

--- variograd_utils/embed_utils.py
+++ variograd_utils/embed_utils.py
@@ -5,7 +5,8 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import normalize
-
+from scipy.sparse import diags
+from mapalign.embed import DiffusionMapEmbedding
 
 def kernelize(A, kernel="linear", scale=50):
     '''
@@ -176,8 +177,108 @@ def reference_laplacian(R, kernel=None, similarity=None, scale=50, laplacian="no
 
 
 
-def joint_embedding(M, R, C=None, n_components=2, kernel=None, similarity=None, scale=50, space=None, 
-                    laplacian="normalized", overwrite=False, svd_kws=None, return_ref=False, rotate=True):
+def diffusion_map(W, n_components, alpha=0.5, diffusion_time=1, random_state=0):
+    """
+    Performs diffusion map embedding on the input matrix W.
+
+    Parameters:
+    ----------
+        W (np.ndarray): Input matrix.
+        n_components (int): Number of embedding dimensions.
+        alpha (float): Diffusion parameter.
+        diffusion_time (int): Diffusion time.
+        random_state (int): Random seed.
+
+    Returns:
+    -------
+        embedding (np.ndarray): Diffusion map embedding.
+        lambdas (np.ndarray): Eigenvalues of the diffusion map.
+    """
+
+    # raise NotImplementedError("This function is not yet implemented")
+
+    # Get the number of samples
+    n = W.shape[0]
+
+    # Compute the normalized Laplacian
+    degree = np.sum(W, axis=1)
+    d_alpha = np.diag(np.sign(degree) * np.abs(degree) ** (-alpha))
+    L_alpha = d_alpha @ W @ d_alpha
+
+    # Compute the random walk Laplacian
+    degree = np.sum(L_alpha, axis=1)
+    d_alpha = np.diag(np.sign(degree) * np.abs(degree) ** -1)
+    M = d_alpha @ L_alpha
+    
+    # Compute the eigendecomposition of the random walk Laplacian
+    M = M - M.mean()
+    SVD = TruncatedSVD(n_components + 1, random_state=random_state).fit(M)
+    lambdas = SVD.singular_values_
+    vectors = SVD.components_.T
+
+    # Compute the diffusion map embedding
+    psi = vectors / np.tile(vectors[:, 0], (vectors.shape[1], 1)).T
+    lambdas[1:] = np.power(lambdas[1:], diffusion_time)
+
+    
+    embedding = psi[:, 1:n_components+1] @ np.diag(lambdas[1:n_components+1], 0)
+    lambdas = lambdas[1:]
+
+    return embedding, lambdas
+
+
+
+def embed_matrix(M, n_components=2, method="svd", method_kws=None):
+    '''
+    Embed a matrix M
+
+    Parameters
+    ----------
+    M : array-like
+        The input matrix
+    n_components : int, optional
+        The number of components to compute (default=2)
+    method : str, optional
+        The method to compute the embedding. Options are "svd", "diffusion" (default="svd")
+        If "svd", uses sklearn.decomposition.TruncatedSVD.
+        If "diffusion", uses mapalign.embed.DiffusionMapEmbedding.
+    method_kws : dict, optional
+        Additional keyword arguments for the embedding algorithm (see method option).
+
+    Returns
+    -------
+    components : array-like
+        The embedding components
+    '''
+
+    N = M.shape[0]
+    if method == "svd":
+
+        if not isinstance(method_kws, dict):
+            method_kws = {}
+        if "random_state" in method_kws.keys():
+            print("'random_state' set to 0 for reproducibility")
+        method_kws["random_state"] = 0
+        method_kws["n_components"] = n_components 
+        
+        return TruncatedSVD(**method_kws).fit_transform(M).T    
+
+    elif method == "diffusion":
+
+        if not isinstance(method_kws, dict):
+            method_kws = {}
+        if "random_state" in method_kws.keys():
+            print("'random_state' set to 0 for reproducibility")
+        method_kws["random_state"] = 0
+        method_kws["n_components"] = n_components
+        # method_kws["affinity"] = "precomputed"
+
+        return DiffusionMapEmbedding(**method_kws).fit_transform(M).T
+
+
+
+def joint_embedding(M, R, C=None, n_components=2, method="svd", kernel=None, similarity=None, scale=50, space=None,
+                    laplacian="normalized", overwrite=False, method_kws=None, return_ref=False, rotate=True):
     '''
     Compute the joint embedding of two matrices M and R
     
@@ -191,9 +292,12 @@ def joint_embedding(M, R, C=None, n_components=2, kernel=None, similarity=None, 
         The correspondence matrix (default=None)
     n_components : int, optional
         The number of components to compute (default=2)
+    method : str, optional
+        The method to compute the embedding. Options are "svd", "diffusion" (default="svd")
+        If "svd", uses sklearn.decomposition.TruncatedSVD.
+        If "diffusion", uses mapalign.embed.DiffusionMapEmbedding.
     kernel : str, optional
         The kernel to apply. Options are "cauchy", "log", "gauss", "linear" (default=None)
-    
     similarity : str, optional
         The method to compute the affinity. Options are "cosine", "correlation" (default=None)
     scale : float, optional
@@ -204,8 +308,9 @@ def joint_embedding(M, R, C=None, n_components=2, kernel=None, similarity=None, 
         The type of Laplacian to compute. Options are "normalized", "unnormalized" (default="normalized")
         overwrite : bool, optional
         Whether to overwrite the input matrices (default=False)
-    svd_kws : dict, optional
-        Additional keyword arguments for TruncatedSVD (default=None)
+    method_kws : dict, optional
+        Additional keyword arguments for the embedding algorithm (see method option).
+        TruncatedSVD (default=None)
     return_ref : bool, optional
         Whether to return the reference embedding (default=False)
     rotate : bool, optional
@@ -231,52 +336,59 @@ def joint_embedding(M, R, C=None, n_components=2, kernel=None, similarity=None, 
 
 
     # Compute the joint Laplacian matrix
-    L = joint_laplacian(M, R, C, kernel=kernel, similarity=similarity, 
-                        scale=scale, space=space, laplacian=laplacian)
+    if method=="svd":
+        L = joint_laplacian(M, R.copy(), C, kernel=kernel, similarity=similarity, 
+                            scale=scale, space=space, laplacian=laplacian)
+        n_components += 1
+    elif method=="diffusion":
+        L = np.vstack([np.hstack([R, C]), 
+                       np.hstack([C.T, M])])
 
     # Compute the eigenvectors and eigenvalues
-    n_components += 1
-    if isinstance(svd_kws, dict) :
-        if "random_state" in svd_kws.keys():
-            print("Truncated SVD's 'random_state' set to 0 for reproducibility")        
-        svd_kws["random_state"] = 0
-        svd_kws["n_components"] = n_components
-    else:
-        svd_kws = {"n_components": n_components, "random_state": 0}
+    components = embed_matrix(L, n_components=n_components, method=method, method_kws=method_kws)
+    A, B = components[:, :N], components[:, N:]
     
-    SVD = TruncatedSVD(**svd_kws).fit(L)
-    A, B = SVD.components_[:, :N], SVD.components_[:, N:]
+    print("Shape of A:", A.shape)
+    print("Shape of B:", B.shape)
+    print("Shape of components:", components.shape)
 
+    # Orthogonality check
+    orthogonality = np.dot(components, components.T)
 
-    # orthogonality check
-    orthogonality = np.dot(SVD.components_, SVD.components_.T)
     np.fill_diagonal(orthogonality, np.nan)
+    print(orthogonality.shape)
     print("Orthogonality check:",
           "\n\tMax:", np.nanmax(orthogonality),
           "\n\tMin:", np.nanmin(orthogonality),
-          "\n\tMean:", np.nanmean(orthogonality))
-
+          "\n\tMean:", np.nanmean(orthogonality),
+          "\n\tSD:", np.nanstd(orthogonality))
 
     # Rotate the joint embedding
     if rotate:
+        L = R if method=="diffusion" else reference_laplacian(R, kernel=kernel, similarity=similarity, scale=scale, laplacian=laplacian)
+        ref = embed_matrix(L, n_components=n_components, method=method, method_kws=method_kws)
 
-        SVD_ref = TruncatedSVD(**svd_kws).fit(reference_laplacian(R, kernel=kernel, similarity=similarity, 
-                                                                  scale=scale, laplacian=laplacian))
-        ref_norm = normalize(SVD_ref.components_, axis=0)
+        ref_norm = normalize(ref, axis=0)
         
         A_norm = normalize(A, axis=0)
+
+        print("Shape of A_norm:", A_norm.shape, "Shape of ref_norm:", ref_norm.shape)
+
         rotation_mat = np.dot(A_norm, ref_norm.T)
 
-        B = np.dot(B.T, rotation_mat)
+        B = np.dot(B.T, rotation_mat).T
+
+        
 
     # Normalize the embeddings
-    B = normalize(B, axis=1)
+    B = normalize(B.T, axis=1)
     A = normalize(A.T, axis=1)
 
+    offset =  1 if method=="svd" else 0
     if return_ref:
-        return B[:, 1:], A[:, 1:]
+        return B[:, offset:], A[:, offset:]
     else:
-        return B[:, 1:]
+        return B[:, offset:]
 
 
 
