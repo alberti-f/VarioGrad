1860b5c785c818128381af078c8273bf9ca60e4a
alberti-f <fnc.alberti@gmail.com>
2024-07-18 12:34:27 +0200

    compute semivariogram

--- 13.geo-embedding_dist.py
+++ 13.geo-embedding_dist.py
@@ -17,7 +17,8 @@ trim = 500
 
 data =  dataset()
 
-row, col = np.triu_indices(500, k=1)
+row, col = np.triu_indices(data.N, k=1)
+
 
 # Calculate embedded geometric distances
 print(f"\nCalculating distances in embedded space")
@@ -27,11 +28,12 @@ for v, vertex in enumerate(LE):
     geo_dists[v] = abs(np.subtract(vertex[row], vertex[col], dtype="float32"))
 print("memory used:", process.memory_info().rss / 1e9)
 
+
 # Calculate functional distances
 print(f"\nCalculating distances in functional space")
-offset = 0 if h == "L" else vertex_info_10k.grayl.size
-hemi = offset + vertex_info_10k[f"gray{h.lower()}"]
+hemi = slice(vertex_info_10k.grayl.size) if h == "L" else slice(vertex_info_10k.grayl.size, None)
 gradients = np.vstack([np.load(subject(id).outpath(f"{id}.REST_FC_embedding.npy"))[hemi, grd] for id in data.subj_list], dtype="float32").T
+gradients -= gradients.mean(axis=1, keepdims=True)
 fun_dists = np.empty(geo_dists.shape)
 for v, vertex in enumerate(gradients):
     fun_dists[v] = abs(np.subtract(vertex[row], vertex[col], dtype="float32"))
@@ -45,16 +47,21 @@ bins = np.array(bins_ol(geo_dists.min(), geo_dists.max(), nbins=nbins, overlap=o
 bin_masks = {}
 for i, (lo, up) in enumerate(bins):
     mask = np.logical_and(geo_dists >= lo, geo_dists < up)
-    if mask.sum(axis=1).min() < 1000:
-        trim_idx = i-1
+    if mask.sum(axis=1).min() < trim:
         break
-    print(f"{i}\t[{lo:.5f}, {up:.5f})\tmin pairs={mask.sum(axis=1).min()}\tmax pairs={mask.sum(axis=1).max()} \tmean % pairs={mask.mean(axis=1).mean()}")
+    print(f"{i+1}\t[{lo:.5f}, {up:.5f})\tmin pairs={mask.sum(axis=1).min()}\
+          max pairs={mask.sum(axis=1).max()} \tmean % pairs={mask.mean(axis=1).mean() * 100:.0f}")
     bin_masks[i] = mask
-print(f"{trim_idx + 1 } bins included")
+print(f"{len(bin_masks.keys())} bins included")
 print("memory used:", process.memory_info().rss / 1e9)
 
 
-# for bin, mask in bin_masks.items():
-#     print(f"Bin {bin}")
-#     fun = fun_dists[mask].mean(axis=1)
-#     print(f"Correlation: {np.corrcoef(geo, fun)[0,1]}")
+# Calculate variograms
+variograd = np.empty([fun_dists.shape[0], len(bin_masks.keys())])
+print("\nCalculating vertex-wise variograms")
+for bin, mask in bin_masks.items():
+    variograd[:, bin] = 0.5 * (np.sum(np.square(fun_dists) * mask, axis=1) / mask.sum(axis=1))
+    print(f"Bin {bin+1}\t\u03B3(h) = {variograd[:, bin].mean():.2f}({variograd[:, bin].std():.2f})")
+print("memory used:", process.memory_info().rss / 1e9)
+
+np.save(data.outpath(f"VarioGrad.{algorithm}.JE{dim}.G{grd}.npy"), variograd)
