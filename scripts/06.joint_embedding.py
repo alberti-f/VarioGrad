c748e27d9e8ab398b607e378b3ee99b6d0f58a04
alberti-f <fnc.alberti@gmail.com>
2024-09-20 14:50:13 +0200

    fix formatting

--- scripts/06.joint_embedding.py
+++ scripts/06.joint_embedding.py
@@ -1,13 +1,15 @@
 # Joint Embedding in parallel
-from variograd_utils import *
-import numpy as np
+
 import sys
 import psutil
+import numpy as np
+from variograd_utils.core_utils import dataset, subject, npz_update
+from variograd_utils.embed_utils import joint_embedding
 
 index = int(sys.argv[1])-1
 
 data = dataset()
-id = data.subj_list[index]
+ID = data.subj_list[index]
 
 n_components = 10
 data = dataset()
@@ -18,7 +20,7 @@ hemi = ["L", "R"]
 params = np.array(np.meshgrid(scale, kernel), dtype="object").T.reshape(-1, 2)
 
 for h in hemi:
-    subj = subject(id)
+    subj = subject(ID)
     subj_points = subj.load_surf(h, 10, type="cortex_midthickness").darrays[0].data
     avg_points = data.load_surf(h, 10, type="cortex_midthickness").darrays[0].data
     C = np.sqrt(np.sum((subj_points - avg_points) ** 2, axis=1), dtype="float32")
@@ -32,15 +34,18 @@ for h in hemi:
     for n, (s, k) in enumerate(params):
         key = f"JE_{k}{int(s)}"
 
-        print(f"\n\nParameter combinaiton {n+1}/{len(params)}\tkey:", key, "\n",)
+        print(f"\n\nParameter combinaiton {n+1}/{len(params)}\tkey:", key, "\n")
         process = psutil.Process()
 
-        all_embeddings[key] = joint_embedding(M, R, C=C, n_components=n_components, kernel=k, scale=s, alignment="rotation")
+        all_embeddings[key] = joint_embedding(M, R, C=C,
+                                              n_components=n_components,
+                                              kernel=k, scale=s,
+                                              alignment="rotation")
 
         print(f"\n\tMemory used: {process.memory_info().rss / (1024 ** 3):.2f} GB\n")
         del process
 
-    filename = subj.outpath(f'{id}.{h}.embeddings.npz')
+    filename = subj.outpath(f'{ID}.{h}.embeddings.npz')
     npz_update(filename,  all_embeddings)
 
     print(f"Output saved in archive {filename} \n")
