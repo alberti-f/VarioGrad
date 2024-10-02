ee39f0083510c129b292749828e26e1965250251
alberti-f <fnc.alberti@gmail.com>
2024-10-02 16:10:04 +0200

    fix formatting and rename

--- scripts/06b.create_geoJE_archives.py
+++ scripts/06b.create_geoJE_archives.py
@@ -1,7 +1,7 @@
-from variograd_utils import *
-import numpy as np
 import os
-
+import numpy as np
+from variograd_utils.core_utils import dataset, subject, npz_update
+from variograd_utils.brain_utils import vertex_info_10k
 
 n_components = 10
 data = dataset()
@@ -15,30 +15,31 @@ zeroarrayr = np.zeros([data.N, vertex_info_10k.grayr.size, n_components])
 # add subject embeddings to the group dictionary
 embeddings_l = {}
 embeddings_r = {}
-for i, id in enumerate(data.subj_list):
+for i, ID in enumerate(data.subj_list):
 
-    subj = subject(id)
+    subj = subject(ID)
 
-    embeddings_subj = np.load(subj.outpath(f"{id}.L.embeddings.npz"))
+    embeddings_subj = np.load(subj.outpath(f"{ID}.L.embeddings.npz"))
     for key, value in embeddings_subj.items():
         if key not in embeddings_l:
             embeddings_l[key] = zeroarrayl.copy()
         embeddings_l[key][i] = value
 
 
-    embeddings_subj = np.load(subj.outpath(f"{id}.R.embeddings.npz"))
+    embeddings_subj = np.load(subj.outpath(f"{ID}.R.embeddings.npz"))
     for key, value in embeddings_subj.items():
         if key not in embeddings_r:
             embeddings_r[key] = zeroarrayr.copy()
         embeddings_r[key][i] = value
-    
 
 # save group embeddings
 npz_update(data.outpath("All.L.embeddings.npz"), embeddings_l)
 npz_update(data.outpath("All.R.embeddings.npz"), embeddings_r)
 
 
-if os.path.exists(subject(id).outpath(f"{id}.L.embeddings.npz")) and os.path.exists(subject(id).outpath(f"{id}.R.embeddings.npz")):
-    for id in data.subj_list:
-        os.remove(subject(id).outpath(f"{id}.L.embeddings.npz"))
-        os.remove(subject(id).outpath(f"{id}.R.embeddings.npz"))
+if os.path.exists(subject(ID).outpath(f"{ID}.L.embeddings.npz")) and os.path.exists(subject(ID).outpath(f"{ID}.R.embeddings.npz")):
+    for ID in data.subj_list:
+        os.remove(subject(ID).outpath(f"{ID}.L.embeddings.npz"))
+        os.remove(subject(ID).outpath(f"{ID}.R.embeddings.npz"))
+
+print("Removed subject embeddings")
