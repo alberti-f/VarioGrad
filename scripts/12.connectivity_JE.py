3a0f533d69bb9e3f0c0c7f7c5778f790ede55a02
alberti-f <fnc.alberti@gmail.com>
2024-10-09 12:40:46 +0100

    use JointEmbedding class also increases rank to 100, uses multiple combinations of alpha and time, and saves output as .npz

--- scripts/12.connectivity_JE.py
+++ scripts/12.connectivity_JE.py
@@ -1,5 +1,5 @@
 import sys
-import psutil#############################################################################
+from itertools import product
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import nibabel as nib
@@ -8,72 +8,67 @@ from variograd_utils.brain_utils import vertex_info_10k
 from variograd_utils.embed_utils import JointEmbedding
 
 
-idx = 1 #int(sys.argv[1])-1
+idx = int(sys.argv[1])-1
 data = dataset()
 ID = data.subj_list[idx]
 subj = subject(ID)
 cortex = np.hstack([vertex_info_10k.grayl, vertex_info_10k.grayr + vertex_info_10k.num_meshl])
 threshold = 95
-n_components = 20
-alpha = 0.5
-diffusion_time = 0
+n_components = 100
+alphas = [0.5, 1]
+diffusion_times = [0, 1]
 
 print(f"Processing subject {ID}")
-
-process = psutil.Process()############################################################
 
 # Load and threshold group average FC matrix
 R = np.load(data.outpath(f"{data.id}.REST_FC.10k_fs_LR.npy")
            ).astype("float32")[:, cortex][cortex, :]
-R[R < 0] = 0
 R[R < np.percentile(R, threshold, axis=0)] = 0
-print(f"\n\tMemory used: {process.memory_info().rss / (1024 ** 3):.2f} GB\n")#############################################
 
 # Load individual timeseries, compute FC, and threshold
 M = nib.load(subject(subj.id).outpath(f"{subj.id}.rfMRI_REST_Atlas_MSMAll.10k_fs_LR.dtseries.nii")
             ).get_fdata().astype("float32")[:, cortex]
 M = np.corrcoef(M.T)
-M[M < 0] = 0
 M[M < np.percentile(M, threshold, axis=0)] = 0
-print(f"\n\tMemory used: {process.memory_info().rss / (1024 ** 3):.2f} GB\n")#############################################
 
-
-# Check for NaNs in the matrices
-if np.any(np.isnan(M)) or np.any(np.isnan(R)):
-    raise ValueError(f"NaN values in FC matrices. Subject {ID} not processed.")
-
-
-# # Compute adjacency matrices
+# Compute correspondance matrix
 C = cosine_similarity(M.T, R.T)
-# W = cosine_similarity(W.T)
-# M = cosine_similarity(M.T)
-
-print("Diffusion map embedding:")
-print(f"\tFC threshold: {threshold}%")
-print(f"\talpha: {alpha}")
-print(f"\tdiffusion time: {diffusion_time}\n\n")
-
 
 # Compute joint diffusion map embedding
-kwargs = {"alpha": alpha, "diffusion_time": diffusion_time}
+print("Diffusion map embedding:")
+print(f"\tFC threshold: {threshold}%")
+print(f"\talphas: {alphas}")
+print(f"\tdiffusion times: {diffusion_times}\n\n")
+
 je = JointEmbedding(method="dme",
                     n_components=n_components,
                     alignment="rotation",
                     random_state=0,
                     copy=True)
-print(f"\n\tMemory used: {process.memory_info().rss / (1024 ** 3):.2f} GB\n")#############################################
 
-embedding, _ = je.fit_transform(M, R, C=C,
-                                affinity="cosine",
-                                method_kwargs=kwargs)
-print(f"\n\tMemory used: {process.memory_info().rss / (1024 ** 3):.2f} GB\n")#############################################
+# Preallocate memory
+kwarg_dict = {f"a{str(a).replace('.', '')}_t{t}": {"alpha": a, "diffusion_time": t} 
+              for a, t in product(alphas, diffusion_times)}
 
-# joint_embedding(M, W, C=C, n_components=n_components, method="diffusion", method_kws=kws, 
-#                             alignment="rotation", normalized=False, overwrite=True)
+embedding_dict = {k: np.zeros([M.shape[0], n_components])
+                  for k in kwarg_dict.keys()}
 
+reference_dict = {k: np.zeros([M.shape[0], n_components])
+                  for k in kwarg_dict.keys()}
 
+# Compute gradients with all combinations of alpha and time
+for key, kwargs in kwarg_dict.items():
+    
+    embedding_dict[key], reference_dict[key] = je.fit_transform(M.T, R.T, C=C,
+                                                                 affinity="cosine",
+                                                                 method_kwargs=kwargs)
+    print(f"\t\N{GREEK SMALL LETTER ALPHA}={kwargs['alpha']} t={kwargs['diffusion_time']} :\t done")
 
-#Save embedding
-np.save(subj.outpath(f"{subj.id}.REST_FC_embedding.npy"), embedding)
+# Save outut
+filename = subj.outpath(f'{ID}.{h}.FC_embeddings.npz')
+npz_update(filename,  embedding_dict)
+print(f"Subject embeddings saved in archive {filename} \n")
 
-print(f"\n\nSubject {ID} embedding saved to {subj.outpath(f'{subj.id}.REST_FC_embedding.npy')}")
+filename = subj.outpath(f'{ID}.{h}.FC_embeddings_refs.npz')
+npz_update(filename,  reference_dict)
+print(f"Reference embeddings saved in archive {filename} \n")
