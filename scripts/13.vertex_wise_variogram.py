ba366f97fcea115f6890129a7a0cfa3855384691
alberti-f <fnc.alberti@gmail.com>
2024-10-23 11:35:24 +0200

    define iterable params within hemisphere loop

--- scripts/13.vertex_wise_variogram.py
+++ scripts/13.vertex_wise_variogram.py
@@ -25,19 +25,19 @@ alpha_time = "a05_t1"
 detrend = False
 
 # Create iterable parameter combinations
-parameters = product(algorithms, ndims, nlagss, max_dist_fracts)
 data =  dataset()
 
 for h in hemispheres:
     print("\nHemisphere:", h)
     hemi = slice(vertex_info_10k.grayl.size) if h == "L" else slice(vertex_info_10k.grayl.size, None)
+    parameters = product(algorithms, ndims, nlagss, max_dist_fracts)
     
     # Loading coordinates in geometric embedding
-    print(f"\n\tLoading coordinates in geometric embedding")
+    print("\n\tLoading coordinates in geometric embedding")
     LE_all = data.load_embeddings(h, "JE")
 
     # Loading coordinates in functional embedding
-    print(f"\n\tLoading coordinates in functional embedding")
+    print("\n\tLoading coordinates in functional embedding")
     load_gradient = lambda ID: np.load(subject(ID).outpath(f'{ID}.FC_embeddings.npz'))[alpha_time][hemi, grd]
     gradients = np.vstack([load_gradient(ID) for ID in data.subj_list], dtype="float32").T
 
@@ -52,7 +52,7 @@ for h in hemispheres:
         # Preallocate arrays
         exp_variograms = np.full([LE.shape[0], nlags], np.nan)
         lag_pairs =  np.full([LE.shape[0], nlags], np.nan)
-        
+
         results = {"exp_variogram": None,
                    "lags": None,
                    "lag_pairs": None,
@@ -65,17 +65,17 @@ for h in hemispheres:
         print(f"\talgorithm: {algorithm}, N dimensions: {ndim}")
         print(f"\t max dist: {max_dist}, N lags: {nlags}, overlap: {overlap*100}%, min pairs: {min_pairs}\n")
         for vtx, (gembd_vtx, fembd_vtx) in enumerate(zip(LE, gradients)):
-        
+
             if gembd_vtx.ndim == 1:
                 gembd_vtx = gembd_vtx.reshape(-1, 1)
-        
+
             if fembd_vtx.ndim == 1:
                 fembd_vtx = fembd_vtx.reshape(-1, 1)
-        
+
             VG = vu.Variogram()
             VG.omndir_variogram(gembd_vtx.copy(), fembd_vtx.copy() / fembd_vtx.std(),
                                 lags, overlap=0, min_pairs=min_pairs)
-        
+
             exp_variograms[vtx] = VG.exp_variogram
             lag_pairs[vtx] = VG.lag_pairs
 
@@ -84,6 +84,7 @@ for h in hemispheres:
                 bounds = (0, [np.nanmax(VG.exp_variogram), 1, np.nanmax(VG.lags)])
                 p0 = [1e-16, 0.5 * np.nanmax(VG.exp_variogram), 0.5 * np.nanmax(VG.lags)]
                 curve_fit_kwargs = {"bounds": bounds, "p0": p0, "method": "trf", "maxfev": 6000}
+
                 try:
                     VG.fit_variogram_model(model=model, curve_fit_kwargs=curve_fit_kwargs)
 
@@ -92,14 +93,15 @@ for h in hemispheres:
                                                                VG.variogram_model["range"],
                                                                VG.variogram_model["sill"],
                                                                VG.variogram_model["r2"]])
+
                 except:
-                    results[f"{model}_model"][vtx] = np.full(5, np.nan)
+                    results[f"{model}_model"][vtx] = np.nan
 
         results["exp_variogram"] = exp_variograms
         results["lags"] = lags
         results["lag_pairs"] = lag_pairs
 
-        # Sve out results
+        # Save out results
         if not os.path.exists(data.outpath("variograms")):
             os.mkdir(data.outpath("variograms"))
         filename = f"variograms/{data.id}.{h}.variogram_{algorithm}.nd{ndim}_nl{nlags}_mxl{int(fract * 100)}.npz"
